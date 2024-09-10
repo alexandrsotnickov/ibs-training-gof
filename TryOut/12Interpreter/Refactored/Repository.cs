626f66115743658b90b2498535f67811b43bc633
alexandrsotnikov <alexandrsotnikov@gmail.com>
2024-09-10 20:13:22 +0700

    moved the "abstract factory" task to the refactored folder

---
+++ TryOut/12Interpreter/Refactored/Repository.cs
@@ -0,0 +1,44 @@
+﻿namespace TryOut.Interpreter.StraightCode {
+    public class Repository {
+        private List<Customer> customer;
+
+        public Repository() {
+            customer = new List<Customer> {
+                                              new Customer {
+                                                               Name = "Acme",
+                                                               Importance = Importance.Global,
+                                                               Country = "USA"
+                                                           },
+                                              new Customer {
+                                                               Name = "Gazprom",
+                                                               Importance = Importance.Global,
+                                                               Country = "Russia"
+                                                           },
+                                              new Customer {
+                                                               Name = "Little pony",
+                                                               Importance = Importance.Regular,
+                                                               Country = "USA"
+                                                           },
+                                              new Customer {
+                                                               Name = "Big fun",
+                                                               Importance = Importance.Returning,
+                                                               Country = "United Kingdome"
+                                                           },
+                                              new Customer {
+                                                               Name = "Birmingham",
+                                                               Importance = Importance.Vip,
+                                                               Country = "United Kingdome"
+                                                           },
+                                              new Customer {
+                                                               Name = "Vasiliy & Co",
+                                                               Importance = Importance.Returning,
+                                                               Country = "Russia"
+                                                           }
+                                          };
+        }
+
+        public List<Customer> GetCustomers() {
+            return customer;
+        }
+    }
+}
